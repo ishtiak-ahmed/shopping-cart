1eba5935ec098c0d42fb95aa6fbed64f0a282ddb
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-01-29 00:05:04 +0600

    Phone function complete

--- cart.js
+++ cart.js
@@ -2,15 +2,23 @@ let CartData = {
     iphone: 1,
     iphonePrice: 1219,
     casing: 1,
-    casingPrice: 59
+    casingPrice: 59,
+    total: 1278
 }
-let total = CartData.iphone * CartData.iphonePrice + CartData.casing * CartData.casingPrice
-console.log(total);
+function total() {
+    let total = CartData.iphone * CartData.iphonePrice + CartData.casing * CartData.casingPrice
+    CartData.total = total;
+};
+// console.log(total);
 
 function updateCart() {
-    console.log('updating cart..');
+    // Update Phone count
     const iphoneCount = document.getElementById('iphoneCount');
+    const phonePrice = document.getElementById('phonePrice')
     iphoneCount.value = CartData.iphone;
+    phonePrice.innerText = (CartData.iphonePrice * CartData.iphone);
+    console.log(CartData.iphonePrice)
+    // 
 }
 function updatePhone(number) {
     console.log("Phone number is", number)
@@ -20,12 +28,19 @@ function updateCasing(number) {
 }
 
 let iphoneReduce = document.querySelector('.iphone .minus');
-iphoneReduce.addEventListener('click', function (event) {
-    CartData.iphone -= 1;
-    console.log(CartData.iphone)
+iphoneReduce.addEventListener('click', function () {
+    if (CartData.iphone > 0) {
+        CartData.iphone -= 1;
+        total();
+        updateCart();
+    }
+    else {
+        console.log('Quantity is already 0.');
+    }
 });
 let iphonePlus = document.querySelector('.iphone .plus');
-iphonePlus.addEventListener('click', function (event) {
+iphonePlus.addEventListener('click', function () {
     CartData.iphone += 1;
+    total();
     updateCart();
 });
