44d8f986bb5eed112deb30b822b658de20b2b241
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-01-29 21:19:29 +0600

    Object oriented method tried

--- cart.js
+++ cart.js
@@ -3,16 +3,28 @@ let CartData = {
     iphonePrice: 1219,
     casing: 1,
     casingPrice: 59,
-    total:
+    subTotal:
         function () {
-            return this.iphone * this.iphonePrice + this.casing * this.casingPrice
-            this.total;
+            return this.iphone * this.iphonePrice + this.casing * this.casingPrice;
         },
     tax: function () {
-        return Math.round(this.total() * .1);
+        return Math.round(this.subTotal() * .1);
+    },
+    phoneCount: function (isIncrease) {
+        if (isIncrease) {
+            this.iphone += 1;
+        } else {
+            if (this.iphone > 0) this.iphone -= 1;
+        }
+    },
+    caseCount: function (isIncrease) {
+        if (isIncrease) {
+            this.casing += 1;
+        } else {
+            if (this.casing > 0) this.casing -= 1;
+        }
     }
 }
-
 
 function updateCart() {
     // Update Phone count
@@ -24,67 +36,23 @@ function updateCart() {
     const total = document.getElementById('total');
     const tax = document.getElementById('tax');
 
-    CartData.total();
-    CartData.tax();
     iphoneCount.value = CartData.iphone;
     caseCount.value = CartData.casing;
     phonePrice.innerText = (CartData.iphonePrice * CartData.iphone);
     casePrice.innerText = (CartData.casingPrice * CartData.casing);
-    subTotal.innerText = CartData.total();
+    subTotal.innerText = CartData.subTotal();
     tax.innerText = CartData.tax();
-    total.innerText = (CartData.total() + CartData.tax());
-    // 
+    total.innerText = (CartData.subTotal() + CartData.tax());
 }
-function updatePhone(number) {
-    console.log("Phone number is", number)
-}
-function updateCasing(number) {
-    console.log("Casing number is", number)
-}
-// Minus count
-let iphoneReduce = document.querySelector('.iphone .minus');
-iphoneReduce.addEventListener('click', function () {
-    if (CartData.iphone > 0) {
-        CartData.iphone -= 1;
-        updateCart();
-    }
-    else {
-        console.log('Quantity is already 0.');
-    }
-});
-// Case Minus count
-let caseMinus = document.querySelector('.casing .minus');
-caseMinus.addEventListener('click', function () {
-    if (CartData.casing > 0) {
-        CartData.casing -= 1;
-        updateCart();
-    }
-    else {
-        console.log('Quantity is already 0.');
-    }
-});
-// Plus count
-let iphonePlus = document.querySelector('.iphone .plus');
-iphonePlus.addEventListener('click', function () {
-    CartData.iphone += 1;
-    updateCart();
-});
-// Case Plus count
-let casePlus = document.querySelector('.casing .plus');
-casePlus.addEventListener('click', function () {
-    CartData.casing += 1;
-    updateCart();
-});
 
+// Phone Minus count
+function updatePhoneCount(isIncrease) {
+    CartData.phoneCount(isIncrease);
+    updateCart();
+};
 
-// Merging function for two button
-function reduceItem(item) {
-    if (CartData.item > 0) {
-        CartData.item -= 1;
-        total();
-        updateCart();
-    }
-    else {
-        console.log('Quantity is already 0.');
-    }
+// Counting Casing
+function updateCaseCount(isIncrease) {
+    CartData.caseCount(isIncrease);
+    updateCart();
 }
